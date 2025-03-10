6ad969766200f87b82b34ce937216130e80ffcf2
eaeber <elysha.aeberhard@gmail.com>
2025-03-10 23:42:42 +0100

    update config

--- vite.config.js
+++ vite.config.js
@@ -1,8 +1,23 @@
 import { defineConfig } from "vite";
 import tailwindcss from "@tailwindcss/vite";
+import { viteStaticCopy } from 'vite-plugin-static-copy';
 
 export default defineConfig({
-  plugins: [tailwindcss()],
+  build: {
+    brotliSize: false,
+  },
+  plugins: [
+    tailwindcss(),
+    viteStaticCopy({
+      targets: [
+          { src: 'src/components/nav.html', dest: 'src/components' },
+          { src: 'src/pages/*.html', dest: 'src/pages' },
+          { src: 'src/assets/*.jpg', dest: 'src/assets'},
+          { src: 'src/assets/*.webp', dest: 'src/assets'}
+      ]
+  })
+
+  ],
   server: {
     port: 5173, 
     open: true, 
